592be43a0e6d09bf37823a91871acfc2323242ec
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-30 23:11:08 +0530

    added sharp api

--- src/workers/thumbnail.worker.ts
+++ src/workers/thumbnail.worker.ts
@@ -1,19 +1,19 @@
 import { Worker } from "bullmq";
-import { generateThumbnail } from "../services/thumbnail.service";
+import { connectDB } from "../config/db";
 
-new Worker(
-  "thumbnail-queue",
-  async (job) => {
-    const { docId, s3Key } = job.data;
-
-    console.log("🔥 Processing job:", job.id);
-
-    await generateThumbnail(docId, s3Key);
-  },
-  {
-    connection: {
-      host: process.env.REDIS_HOST || "127.0.0.1",
-      port: Number(process.env.REDIS_PORT) || 6379,
+(() => {
+  connectDB();
+  new Worker(
+    "thumbnail-queue",
+    async (job) => {
+      console.log("Processing job:", job.id);
+      // your logic
     },
-  }
-);
+    {
+      connection: {
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: Number(process.env.REDIS_PORT) || 6379,
+      },
+    },
+  );
+})();
