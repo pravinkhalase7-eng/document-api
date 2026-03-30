a7513c6214c170602dfd5a69f3c02d474e828e64
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-30 13:45:11 +0530

    added auth service

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,6 +7,7 @@ import { getUserById } from "../services/user.service";
 export const register = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
+  console.log('user is created...')
   const existing = await getUserByEmail(email);
   if (existing) {
     return res.status(400).json({ message: "User already exists" });
