15a9bd5f748238e7503b2d07d6b4b4535f5a40a3
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-30 15:18:59 +0530

    added logs

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,14 +3,16 @@ import { verifyToken } from "../utils/jwt";
 export const authMiddleware = (req: any, res: any, next: any) => {
     console.log('checkign auth');
   const token = req.headers.authorization?.split(" ")[1];
-
+  console.log('token', token)
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
     const decoded: any = verifyToken(token);
+    console.log('decoded', decoded)
     req.user = decoded;
     next();
-  } catch {
+  } catch(err) {
+    console.log('error', err)
     res.status(401).json({ message: "Invalid token" });
   }
 };
