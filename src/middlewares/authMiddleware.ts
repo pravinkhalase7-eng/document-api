c962ef24b7d3f334ae62e5549fbb82809b6db658
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-30 13:49:10 +0530

    added route

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,15 @@
+import { verifyToken } from "../utils/jwt";
+
+export const authMiddleware = (req: any, res: any, next: any) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded: any = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,12 +5,13 @@ import {
   getUserById,
   deleteUser,
 } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.delete("/:id", deleteUser);
+router.post("/", authMiddleware, createUser);
+router.get("/", authMiddleware, getUsers);
+router.get("/:id",authMiddleware,  getUserById);
+router.delete("/:id",authMiddleware, deleteUser);
 
 export default router;
