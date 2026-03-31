c535df651b6b7177f0f76e11014f19ba2fca2606
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-31 23:33:05 +0530

    added all docs

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,33 +1,49 @@
-import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  UploadPartCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
 
 import { BUCKET, s3 } from "../utils/s3";
 import { getUserById } from "./user.service";
 
-
-
 export const generateUploadUrl = async (
   fileName: string,
   mimeType: string,
-  userId: string
+  userId: string,
+  isLargeFile: string,
+  uploadId: string,
+  partNumber: any,
 ) => {
-
-   const userInfo = await getUserById(userId);
-   console.log('bucket', userInfo?.folderName)
+  const userInfo = await getUserById(userId);
+  console.log("bucket", userInfo?.folderName);
   const key = `documents/${userInfo?.folderName}/${uuidv4()} - ${fileName}`;
 
-  const command = new PutObjectCommand({
-    Bucket: BUCKET,
-    Key: key,
-    ContentType: mimeType,
-  });
-
-  const uploadUrl = await getSignedUrl(s3, command, {
-    expiresIn: 60 * 5, // 5 minutes
-  });
-
   const fileUrl = `https://${BUCKET}.s3.ap-south-1.amazonaws.com/${key}`;
+  let uploadUrl = "";
+
+  if (isLargeFile) {
+    const command = new UploadPartCommand({
+      Bucket: BUCKET,
+      Key: key,
+      UploadId: uploadId,
+      PartNumber: partNumber,
+    });
+    uploadUrl = await getSignedUrl(s3, command, {
+      expiresIn: 60 * 5, // 5 minutes
+    });
+  } else {
+    const command = new PutObjectCommand({
+      Bucket: BUCKET,
+      Key: key,
+      ContentType: mimeType,
+    });
+    uploadUrl = await getSignedUrl(s3, command, {
+      expiresIn: 60 * 5, // 5 minutes
+    });
+  }
 
   return {
     uploadUrl,
