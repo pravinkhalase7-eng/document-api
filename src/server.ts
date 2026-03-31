4508fde03e1dc4fcef1db74a8f66f67980e2e200
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-03-31 23:59:53 +0530

    added all docs

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // 🔥 Connect DB
 connectDB();
