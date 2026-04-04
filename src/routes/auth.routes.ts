cfccdd3bf31c256a41fd4769d7232dd2e00360fa
Pravin Khalase <pravinkhalase@Pravins-MacBook-Pro.local>
2026-04-04 22:17:59 +0530

    added auth route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/me", getUserInfo);
-router.post("/callback", callback);
+router.get("/callback", callback);
 
 export default router;
